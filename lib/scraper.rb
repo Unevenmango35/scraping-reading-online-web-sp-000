e031635223efaa1259387e7c420fc877d81653a0
Rosie Choi <rosiechoimanso@gmail.com>
2021-01-17 00:05:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,4 +7,7 @@ doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 doc.css(".headline-26OIBN").text
 puts doc.css(".headline-26OIBN")
 
-doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+courses.each do |course|
+  puts course.text.strip
+end
